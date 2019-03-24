723d4d1e0e1037a9d623649ee3839dde3c26bf0b
Joanna White <joanna1016@hotmail.com>
2019-03-24 02:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
 	var customer = "The line is currently: ";
     for(var i = 0; i < katzDeliLine.length; i++) {
-      customer = customer.concat(`${i + 1}. ${katzDeliLine[i]}, `);
+      if (i + 1 != katz.length) {
+        customer = customer.concat(`${i + 1}. ${katzDeliLine[i]}, `);
+      } else {
+        customer = customer.concat(`${i + 1}. ${katzDeliLine[i]}.`);
+      }
     } return customer
   } else {
     return "The line is currently empty.";
